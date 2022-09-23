4a9740dd30f25cd1e88a6a4b6b355c62d5cbec54
Matheus Popolin <matheuspopolin@hotmail.com>
2022-09-22 21:34:01 -0300

    Primeira versão

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,8 @@
-/* Desenvolva seu código aqui... */
+const buttonToggle = document.querySelectorAll("[data-modal-control]");
+
+for(let i=0; i<buttonToggle.length; i++){
+    buttonToggle[i].addEventListener("click", function(){
+        let valueDataModalControl = buttonToggle[i].getAttribute("data-modal-control");
+        document.getElementById(valueDataModalControl).classList.toggle("show_modal");        
+    })
+}
